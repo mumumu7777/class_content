242b4acc90aef3548dbdf6de4b2583a5f9fd97d9
mumumu7777 <kevin3571630@gmail.com>
2022-11-14 16:46:31 +0800

    qq2

--- 11_11eshop11_14/ISpan.Estore/ISpan.Estore/UsersForm.Designer.cs
+++ 11_11eshop11_14/ISpan.Estore/ISpan.Estore/UsersForm.Designer.cs
@@ -29,11 +29,89 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.usersFormBindingSource = new System.Windows.Forms.BindingSource(this.components);
+			this.uservmBindingSource = new System.Windows.Forms.BindingSource(this.components);
+			this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.accountDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.passWordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.usersFormBindingSource)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.uservmBindingSource)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AutoGenerateColumns = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.accountDataGridViewTextBoxColumn,
+            this.passWordDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn});
+			this.dataGridView1.DataSource = this.uservmBindingSource;
+			this.dataGridView1.Location = new System.Drawing.Point(39, 218);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(457, 150);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// usersFormBindingSource
+			// 
+			this.usersFormBindingSource.DataSource = typeof(ISpan.Estore.UsersForm);
+			// 
+			// uservmBindingSource
+			// 
+			this.uservmBindingSource.DataSource = typeof(ISpan.Estore.Models.ViewModels.uservm);
+			// 
+			// idDataGridViewTextBoxColumn
+			// 
+			this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+			this.idDataGridViewTextBoxColumn.HeaderText = "id";
+			this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+			// 
+			// accountDataGridViewTextBoxColumn
+			// 
+			this.accountDataGridViewTextBoxColumn.DataPropertyName = "Account";
+			this.accountDataGridViewTextBoxColumn.HeaderText = "Account";
+			this.accountDataGridViewTextBoxColumn.Name = "accountDataGridViewTextBoxColumn";
+			// 
+			// passWordDataGridViewTextBoxColumn
+			// 
+			this.passWordDataGridViewTextBoxColumn.DataPropertyName = "PassWord";
+			this.passWordDataGridViewTextBoxColumn.HeaderText = "PassWord";
+			this.passWordDataGridViewTextBoxColumn.Name = "passWordDataGridViewTextBoxColumn";
+			// 
+			// nameDataGridViewTextBoxColumn
+			// 
+			this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+			this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+			this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+			// 
+			// UsersForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "UsersForm";
 			this.Text = "UsersForm";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.usersFormBindingSource)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.uservmBindingSource)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn accountDataGridViewTextBoxColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn passWordDataGridViewTextBoxColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+		private System.Windows.Forms.BindingSource uservmBindingSource;
+		private System.Windows.Forms.BindingSource usersFormBindingSource;
 	}
 }
